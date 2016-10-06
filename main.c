db8a8589f9e7cf7ed65a3f0cfe777045b1472c28
taekgu <taekgu04@naver.com>
2016-10-06 01:05:18 -0700

    test

--- main.c
+++ main.c
@@ -5,11 +5,8 @@ extern int foo2(int a, int b);
 
 int main()
 {
-<<<<<<< HEAD
+
 	int a = 14;
-=======
-	int a = 14;
->>>>>>> bug123
 	int b = 4;
 	printf("show foo1 : %d , show foo2 : %d\n",foo1(a,b),foo2(a,b));
 	return 1;
