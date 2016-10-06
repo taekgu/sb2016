ab6993ab8f4162420c114d9084003f23a18668a2
taekgu <taekgu04@naver.com>
2016-10-06 00:33:27 -0700

    first test

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+extern int foo1(int a, int b);
+extern int foo2(int a, int b);
+
+int main()
+{
+	int a = 5;
+	int b = 4;
+	printf("show foo1 : %d , show foo2 : %d\n",foo1(a,b),foo2(a,b));
+	return 1;
+}
